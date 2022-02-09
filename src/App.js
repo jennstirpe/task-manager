65ad85c21bf7edbecc60e07cfef902f6b672b87d
jstirpe <jennstirpe@yahoo.com>
2022-02-08 22:43:38 -0500

    Initial commit. Created basic layout for app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Task Tracker</h1>
+        <button className="add">Add Task</button>
+        <form action="">
+
+        </form>
       </header>
+      <main>
+        <div className="tasks-list">
+          <div className="task">
+            <h2>Clean Room</h2>
+            <p>garbage, vacuum, dust</p>
+            <p>Wednesday</p>
+            <button className="edit">Edit</button>
+            <button className="delete">Delete</button>
+          </div>
+          <div className="task">
+            <h2>Laundry</h2>
+            <p>bedding, work clothes, sweaters</p>
+            <p>Tuesday</p>
+            <button className="edit">Edit</button>
+            <button className="delete">Delete</button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
