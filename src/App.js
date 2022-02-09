afe85b44dad078c92208d8e2b55529066fa2023b
jstirpe <jennstirpe@yahoo.com>
2022-02-08 23:49:23 -0500

    Added basic styling for layout and colors

--- src/App.js
+++ src/App.js
@@ -13,18 +13,48 @@ function App() {
       <main>
         <div className="tasks-list">
           <div className="task">
-            <h2>Clean Room</h2>
-            <p>garbage, vacuum, dust</p>
-            <p>Wednesday</p>
-            <button className="edit">Edit</button>
-            <button className="delete">Delete</button>
+            <p className="due-day">Wednesday</p>
+            <div className="task-info">
+              <h2>Clean Room</h2>
+              <p>garbage, vacuum, dust</p>
+            </div>
+            <div className="task-modify">
+              <button className="edit">Edit</button>
+              <button className="delete">Delete</button>
+            </div>
           </div>
           <div className="task">
-            <h2>Laundry</h2>
-            <p>bedding, work clothes, sweaters</p>
-            <p>Tuesday</p>
-            <button className="edit">Edit</button>
-            <button className="delete">Delete</button>
+            <p className="due-day">Tuesday</p>
+            <div className="task-info">
+              <h2>Laundry</h2>
+              <p>bedding, work clothes, sweaters</p>
+            </div>
+            <div className="task-modify">
+              <button className="edit">Edit</button>
+              <button className="delete">Delete</button>
+            </div>
+          </div>
+          <div className="task">
+            <p className="due-day">Saturday</p>
+            <div className="task-info">
+              <h2>Laundry</h2>
+              <p>jdoelsjdnejdoelsjdnejdoelsjdnejdoelsjdnejdoelsjdnejdoelsjdnejdoelsjdnejdoelsjdnejdoelsjdnejdoelsjdne</p>
+            </div>
+            <div className="task-modify">
+              <button className="edit">Edit</button>
+              <button className="delete">Delete</button>
+            </div>
+          </div>
+          <div className="task">
+            <p className="due-day">Thursday</p>
+            <div className="task-info">
+              <h2>Laundry</h2>
+              <p>SET MAX LENGTH FOR THIS INPUT</p>
+            </div>
+            <div className="task-modify">
+              <button className="edit">Edit</button>
+              <button className="delete">Delete</button>
+            </div>
           </div>
         </div>
       </main>
